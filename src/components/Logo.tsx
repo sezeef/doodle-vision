8019defaa388cc76aee757e7d7d6e9e4a53aba0e
sezeef <sezeef.h@gmail.com>
2023-07-28 23:28:37 +0300

    feat: Optimized logo & Changed favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 type Props = {
@@ -9,17 +8,13 @@ type Props = {
 
 export default function Logo({ className, width, height }: Props) {
   return (
-    <div
-      className="flex items-center justify-center overflow-hidden rounded-full border-2 border-slate-800 bg-black"
-      style={{ width, height }}
-    >
-      <Image
-        src={"/Cute-Eye.jpg"}
-        alt="Logo"
-        width={width}
-        height={height}
-        className={cn(className)}
-      />
-    </div>
+    <Image
+      src={"/Cute-Eye_circle-cropped_512.png"}
+      alt="Doodle Vision Logo"
+      width={width}
+      height={height}
+      priority={true}
+      className={className}
+    />
   );
 }
